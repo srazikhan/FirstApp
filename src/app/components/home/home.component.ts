820b16d660174471e6e364172e49ec81a4890c36
nadeemsoft <nadeemashrafi38@gmail.com>
2022-06-24 09:50:43 +0530

    data binding examples

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,12 +7,21 @@ import { AuthService } from 'src/app/core/services/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  address:string = "Hyderabad Telangana 50045";
+  countries:string[] =['india','America'];
+  imgTitle:string = "image ex";
+  imgUrl:string = "https://static.digit.in/product/284c81498a93a82db7b5db6600494c20bee4f18e.jpeg";
+  imgAlt:string = "Image";
   constructor(private authService:AuthService) {
     this.authService.welcomeMessage();
   }
 
   ngOnInit(): void {
+  }
+
+  showMessage(countries:any){
+    console.log("Welcome Good Morning");
+    console.log(countries);
   }
 
 }
