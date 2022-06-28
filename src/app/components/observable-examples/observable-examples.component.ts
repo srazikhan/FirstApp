b34c5deab52f75d3a1fe0dff446978a3d1f8b3b6
nadeemsoft <nadeemashrafi38@gmail.com>
2022-06-28 09:33:33 +0530

    observable

--- src/app/components/observable-examples/observable-examples.component.ts
+++ src/app/components/observable-examples/observable-examples.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable,of } from 'rxjs';
+import { BehaviorSubject, concat, forkJoin, from, interval, merge, Observable, of, range, Subject, Subscription, timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 
 @Component({
   selector: 'app-observable-examples',
@@ -7,8 +10,8 @@ import { Observable,of } from 'rxjs';
   styleUrls: ['./observable-examples.component.scss']
 })
 export class ObservableExamplesComponent implements OnInit {
-
-  constructor() { 
+  userList: any;
+  constructor() {
     // const obs1 = new Observable((observer)  => {
     //   observer.next(42);
     //   observer.next(100);
@@ -22,18 +25,87 @@ export class ObservableExamplesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-  this.observableEx();
+    this.observableEx();
   }
 
-  observableEx(){
-    const obsByOf1 = of(123,344,567);
-    const obsByOf2= of("hi","bye","hello");
-    obsByOf1.subscribe((res:any)=>{
+  observableEx() {
+    //creating observable using from operator
+    // const obsByFrom = from(['sajjad','razi']);
+    // obsByFrom.subscribe((res:any)=>{
+    //   console.log(res)
+    // })
+
+    //creating observable using of operator
+    // const obsByOf1 = of(123,344,567);
+    // obsByOf1.subscribe((res:any)=>{
+    //   console.log(res)
+    // })
+
+    //creating observable using range operator
+    //  const obsByRange = range(1,10);
+    //  obsByRange.pipe(map(el => el+2)).subscribe((res:any)=>{
+    //    console.log(res)
+    //  })
+
+    //creating observable using interval operator
+    //  const obsByInterVel = interval(1000);
+    // const timeSub:Subscription = obsByInterVel.subscribe((res:any)=>{
+    //    console.log(res)
+    //    if(res == 10){
+    //     timeSub.unsubscribe();
+    //    }
+    //  })
+
+    //creating observable using timer operator
+    //  const obsByIimer = timer(5000);
+    //   obsByIimer.subscribe((res:any)=>{
+    //    console.log("i am good" + res)
+    //  })
+
+
+    //creating observable using ajax operator
+    //  const obsByAjax = ajax('https://jsonplaceholder.typicode.com/posts');
+    //  obsByAjax.subscribe((res:any)=>{
+    //    console.log(res.response)
+    //    this.userList = res.response
+    //  })
+
+    //merging observable using concat operator
+    // const sequence1 = range(1, 10);
+    // const sequence2 = range(12, 20);
+    // const result = concat(sequence1, sequence2);
+    // result.subscribe((res:any)=>{
+    //   console.log(res)
+    // })
+
+    //merging observable using forkjoin operator
+    // const obs1 = ajax('https://jsonplaceholder.typicode.com/posts');
+    // const obs2 = ajax('https://jsonplaceholder.typicode.com/posts');
+    // const result = forkJoin([obs1,obs2]);
+    // result.subscribe((res:any)=>{
+    //   console.log(res)
+    // })
+
+  //merging observable using merge operator
+    // const myrange1 = range(0, 5);
+    // const myrange2 = range(10, 20);
+    // const merged = merge(myrange1, myrange2);
+    // merged.subscribe(x => console.log(x));
+
+     //creating observable using subject operator
+    const sub1 = new Subject();
+    const sub2 = new BehaviorSubject<any>('I m good');
+
+    sub1.subscribe((res:any)=>{
+      console.log("***"+res)
+    })
+
+    sub2.subscribe((res:any)=>{
       console.log(res)
     })
-    obsByOf2.subscribe((res:any)=>{
-      console.log(res)
-    })
+   
   }
+
+
 
 }
